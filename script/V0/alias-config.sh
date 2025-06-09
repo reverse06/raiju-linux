f9f84c50afdeb4e15faffe882b2b6f3735ec4462
reverse06 <palemoon06@protonmail.com>
2025-06-09 22:49:26 +0200

    fix(script): update alias-config and remove nvim-config

--- script/V0/alias-config.sh
+++ script/V0/alias-config.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 set -euo pipefail
 
-tee ~/.bashrc <<'EOF'
-# Custom alias
+echo "Configuring .bashrc..."
+
+cat > ~/.bashrc <<'EOF'
+[[ $- != *i* ]] && return
+
+HISTCONTROL=ignoredups:erasedups
+HISTSIZE=10000
+HISTFILESIZE=20000
+shopt -s histappend
+
+export LS_OPTIONS='--color=auto'
+eval "$(dircolors -b)"
+alias ls='ls $LS_OPTIONS'
+alias grep='grep --color=auto'
+
+PS1='\u@\h:\w\$ '
+
+if [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+fi
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# Alias Raiju
+# -----------------------------------
+# Custom aliases (Raiju)
 alias light='sudo'
 alias eclipse='sudo pacman -S'
 alias goto='cd'
@@ -12,7 +38,13 @@ alias r='reboot'
 alias q='poweroff'
 alias k='sudo pacman -Syu'
 alias moon='grc ping -c 4 1.1.1.1'
+# -----------------------------------
+
+neofetch --config ~/.config/neofetch/config.conf
+
 EOF
 
 source ~/.bashrc
+
+echo ".bashrc configured."
 
